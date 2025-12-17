b6199adc1de62b2ae9118c5b38a8ebfb953d3c4d
lusalord <lusalord63@gmail.com>
2025-12-17 15:31:07 +0900

    feat : blog gle

--- lusalord_blog/JS/data.js
+++ lusalord_blog/JS/data.js
@@ -9,18 +9,32 @@ export const projects = [
     },
     {
         title: "1학년 1학기 개인 프로젝트",
-        desc: "HTML, CSS, JS로 만든 개인 포트폴리오 및 블로그 웹사이트입니다.",
+        desc: "유니티를 이용하여 제작한 로그라이크 던전 게임입니다.",
         tags: ["Unity", "C#"],
         
         url: "Blog_post/post-3-Personal-Project.html" 
+    },
+    {
+        title: "2025년 비공식 게임잼",
+        desc: "게임잼에서 제작한 윈도우를 테마로 한 퍼즐 게임입니다.",
+        tags: ["Unity", "C#"],
+        
+        url: "Blog_post/post-4-Team-Project-FlipFlop.html" 
     }
 ];
 
 // 블로그 글 목록 데이터
 export const blogPosts = [
     {
+        title: "2025년 비공식 게임잼 팀 프로젝트 개발",
+        date: "2025-12-17",
+        summary: "개인 프로젝트 소개와 개발한 기능 설명",
+    
+        url: "Blog_post/post-4-Team-Project-FlipFlop.html" 
+    },
+    {
         title: "1학년 1학기 개인 프로젝트 개발",
-        date: "2025-12-15",
+        date: "2025-12-16",
         summary: "개인 프로젝트 소개와 개발한 기능 설명",
     
         url: "Blog_post/post-3-Personal-Project.html" 
